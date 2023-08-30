77f42996d72555a4c42f9372961b4de2c5e94cf0
Manfred Graf <manfred.graf91@gmail.com>
2023-08-30 23:44:39 +0200

    chore: added icon

--- mobro-plugin-cli/Model/PluginMeta.cs
+++ mobro-plugin-cli/Model/PluginMeta.cs
@@ -1,6 +1,10 @@
 ﻿namespace MoBro.Plugin.Cli.Model;
 
-internal sealed record PluginMeta(string Name, string AssemblyName, string Version)
+internal sealed record PluginMeta(
+  string Name,
+  string AssemblyName,
+  string Version
+)
 {
   public PluginMeta(string Name, string AssemblyName, Version Version)
     : this(Name, AssemblyName, $"{Version.Major}.{Version.Minor}.{Version.Build}")
--- mobro-plugin-cli/Program.cs
+++ mobro-plugin-cli/Program.cs
@@ -4,19 +4,11 @@ using MoBro.Plugin.Cli.CliArgs;
 using MoBro.Plugin.Cli.Helper;
 using Refit;
 
-// TODO just temporary for testing
-args = new[] { "publish", "C:\\dev\\mobro-data-plugins\\Plugin.Template" };
-// args = new[] { "install", "C:\\dev\\mobro-data-plugins\\Plugin.Template" };
-// args = new[] { "install", "C:\\dev\\mobro-plugin-cli\\mobro-plugin-cli\\bin\\Debug\\net7.0\\example_plugin_0.0.1.zip" };
 
 try
 {
   Parser.Default
-    .ParseArguments<
-      PublishArgs,
-      InstallArgs,
-      MarketplacePublishArgs
-    >(args)
+    .ParseArguments<PublishArgs, InstallArgs, MarketplacePublishArgs>(args)
     .WithParsed<PublishArgs>(PublishAction.Invoke)
     .WithParsed<InstallArgs>(InstallAction.Invoke)
     .WithParsed<MarketplacePublishArgs>(MarketplacePublishAction.Invoke);
