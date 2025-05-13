d7870e3455f903eb5fe4b00d3f420b70d0db7429
Manfred Graf <manfred.graf91@gmail.com>
2025-05-13 22:17:55 +0200

    fix: rename project to reflect assembly name and fix name of generated .exe

--- MoBro.Plugin.CLI/Program.cs
+++ MoBro.Plugin.CLI/Program.cs
@@ -4,6 +4,15 @@ using MoBro.Plugin.Cli.CliArgs;
 using MoBro.Plugin.Cli.Helper;
 using Refit;
 
+// TODO just temporary for testing
+// args = new[] { "publish", "C:\\dev\\mobro-data-plugins\\Plugin.Template" };
+// args = new[] { "install", "C:\\dev\\mobro-data-plugins\\Plugin.Template" };
+// args = new[] { "install", "C:\\dev\\mobro-plugin-cli\\mobro-plugin-cli\\bin\\Debug\\net7.0\\example_plugin_0.0.1.zip" };
+// args = new[] { "install", "D:\\published_test\\modbros_mobrohardwaremonitor_1.0.0.zip" };
+// args = new[] { "marketplace-publish", "--api-key", "ValidApiKey", "--dev", "D:\\published_test\\modbros_mobrohardwaremonitor_1.0.0.zip" };
+// args = new[] { "marketplace-update-store-page", "--api-key", "ValidApiKey", "--dev","--store-page-file", "D:\\repos\\mobro-data-plugins\\Plugin.OpenWeather\\marketplace.md", "modbros_openweather"};
+// args = new[] { "create", "test" };
+// args = new[] { "publish", "-o", "D:\\published_test", "D:\\repos\\mobro-data-plugins\\Plugin.MoBroHardwareMonitor" };
 
 try
 {
